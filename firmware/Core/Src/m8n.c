41a92d6290e8649a8454bb979969c0414d586cd6
Vava-2549 <vavap@my.yorku.ca>
2026-03-29 23:13:22 -0400

    Added comments to m8n.c

--- firmware/Core/Src/m8n.c
+++ firmware/Core/Src/m8n.c
@@ -1,8 +1,7 @@
 /*
  * m8n.c
  *
- *  Created on: 16 มี.ค. 2569
- *      Author: pkuna
+ * Source code for the m8n gps
  */
 
 #include "main.h"
@@ -10,15 +9,15 @@
 
 int readData(void) {
 	while (1) {
-		/* UART data receiver */
+		// UART data receiver
 		HAL_UART_Receive(&huart2, &gpsData, gpsDataSize, 1000);
 		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_2);
 		HAL_Delay(1000);
-
+		// Push data into buffer
 		if (circBuffPush(&gpsBuffer, gpsData)) {
 			printf("yes");
 		}
-
+		// Pop data into CAN_TX
 		if (circBuffPop(&gpsBuffer, &CAN_TX)) {
 			printf("yes");
 		}
