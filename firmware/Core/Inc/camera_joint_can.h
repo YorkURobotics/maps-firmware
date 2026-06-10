f1e761718e608f15efca516e9b4685ad838bb93d
Mykola Bohomaz <bohomazmykola@gmail.com>
2026-06-09 20:41:48 -0400

    changed can layer architecture

--- firmware/Core/Inc/camera_joint_can.h
+++ firmware/Core/Inc/camera_joint_can.h
@@ -8,39 +8,13 @@
 /*
  * Extended 29-bit CAN IDs
  */
-#define CAN_CAMERA_JOINT_SET_ANGLES_ID        0x0E110C10U
-#define CAN_CAMERA_JOINT_GET_STATUS_ID        0x0E110C11U
-#define CAN_CAMERA_JOINT_STATUS_ID            0x0E110C12U
-#define CAN_CAMERA_JOINT_CENTER_ID            0x0E110C13U
-
-/*
- * Panorama mode:
- * Payload is 3 bytes:
- *   Byte 0   = axis selector
- *   Byte 1-2 = angle in tenths of degrees, little-endian
- */
-#define CAN_CAMERA_JOINT_PANORAMA_MOVE_ID     0x0E110C20U
-
-/*
- * Axis selector values
- */
-#define CAMERA_JOINT_AXIS_X                   0x00U
-#define CAMERA_JOINT_AXIS_Y                   0x01U
+#define CAN_SERVO_X        0x0E110C10U
+#define CAN_SERVO_Y        0x0E110C11U
 
 /*
  * Payload sizes
  */
-#define CAN_CAMERA_JOINT_SET_ANGLES_DLC       4U
-#define CAN_CAMERA_JOINT_PANORAMA_MOVE_DLC    3U
-#define CAN_CAMERA_JOINT_STATUS_DLC           8U
-
-/*
- * Status flags
- */
-#define CAMERA_JOINT_STATUS_OK                0x00U
-#define CAMERA_JOINT_STATUS_ANGLE_CLAMPED     0x01U
-#define CAMERA_JOINT_STATUS_CAN_TIMEOUT       0x02U
-#define CAMERA_JOINT_STATUS_ERROR             0x04U
+#define CAN_SERVO_DLC       2U
 
 void CameraJointCAN_Init(CameraJoint_t *joint);
 
@@ -50,7 +24,5 @@ void CameraJointCAN_ProcessFrame(
     uint8_t *data,
     uint8_t len
 );
-
-HAL_StatusTypeDef CameraJointCAN_SendStatus(CAN_HandleTypeDef *hcan);
 
 #endif
--- firmware/Core/Src/camera_joint_can.c
+++ firmware/Core/Src/camera_joint_can.c
@@ -45,11 +45,10 @@ void CameraJointCAN_ProcessFrame(CAN_HandleTypeDef *hcan, uint32_t id, uint8_t *
 
     switch (id)
     {
-        case CAN_CAMERA_JOINT_SET_ANGLES_ID:
+        case CAN_SERVO_X:
         {
             /*
              * Byte 0-1 = pan angle in tenths of degrees
-             * Byte 2-3 = tilt angle in tenths of degrees
              */
             if (len < CAN_CAMERA_JOINT_SET_ANGLES_DLC)
             {
@@ -57,85 +56,28 @@ void CameraJointCAN_ProcessFrame(CAN_HandleTypeDef *hcan, uint32_t id, uint8_t *
             }
 
             int16_t pan_tenths = CAN_ReadInt16LE(data[0], data[1]);
-            int16_t tilt_tenths = CAN_ReadInt16LE(data[2], data[3]);
 
             float pan_deg = TenthsToDegrees(pan_tenths);
+
+            CameraJoint_SetPan(camera_joint_can_instance, pan_deg);
+            break;
+        }
+
+    case CAN_SERVO_Y:
+        {
+             /*
+             * Byte 0-1 = tilt angle in tenths of degrees
+             */
+            if (len < CAN_SERVO_DLC)
+            {
+                return;
+            }
+
+            int16_t tilt_tenths = CAN_ReadInt16LE(data[0], data[1]);
+
             float tilt_deg = TenthsToDegrees(tilt_tenths);
 
-            CameraJoint_SetAngles(camera_joint_can_instance, pan_deg, tilt_deg);
-            break;
-        }
-
-        case CAN_CAMERA_JOINT_PANORAMA_MOVE_ID:
-        {
-            /*
-             * Byte 0:
-             *   0x00 = X axis / pan
-             *   0x01 = Y axis / tilt
-             *
-             * Byte 1-2:
-             *   angle in tenths of degrees, little-endian
-             */
-            if (len < CAN_CAMERA_JOINT_PANORAMA_MOVE_DLC)
-            {
-                return;
-            }
-
-            uint8_t axis = data[0];
-
-            int16_t angle_tenths = CAN_ReadInt16LE(data[1], data[2]);
-            float angle_deg = TenthsToDegrees(angle_tenths);
-
-            if (angle_deg < 0.0f)
-            {
-                angle_deg = 0.0f;
-            }
-
-            if (angle_deg > 270.0f)
-            {
-                angle_deg = 270.0f;
-            }
-
-            if (axis == CAMERA_JOINT_AXIS_X)
-            {
-                CameraJoint_SetTilt(camera_joint_can_instance, 135.0f);
-                CameraJoint_SetPan(camera_joint_can_instance, angle_deg);
-            }
-            else if (axis == CAMERA_JOINT_AXIS_Y)
-            {
-                CameraJoint_SetPan(camera_joint_can_instance, 135.0f);
-                CameraJoint_SetTilt(camera_joint_can_instance, angle_deg);
-            }
-            else
-            {
-                return;
-            }
-
-            break;
-        }
-
-        case CAN_CAMERA_JOINT_CENTER_ID:
-        {
-            float pan_center =
-                (camera_joint_can_instance->pan_min_deg +
-                 camera_joint_can_instance->pan_max_deg) / 2.0f;
-
-            float tilt_center =
-                (camera_joint_can_instance->tilt_min_deg +
-                 camera_joint_can_instance->tilt_max_deg) / 2.0f;
-
-            CameraJoint_SetAngles(
-                camera_joint_can_instance,
-                pan_center,
-                tilt_center
-            );
-
-            break;
-        }
-
-        case CAN_CAMERA_JOINT_GET_STATUS_ID:
-        {
-          CameraJointCAN_SendStatus(hcan);
+            CameraJoint_SetTilt(camera_joint_can_instance, tilt_deg);
             break;
         }
 
@@ -144,33 +86,3 @@ void CameraJointCAN_ProcessFrame(CAN_HandleTypeDef *hcan, uint32_t id, uint8_t *
     }
 }
 
-HAL_StatusTypeDef CameraJointCAN_SendStatus(CAN_HandleTypeDef *hcan)
-{
-    if (camera_joint_can_instance == 0)
-    {
-        return HAL_ERROR;
-    }
-
-    uint8_t data[8] = {0};
-
-    int16_t pan_tenths =
-        DegreesToTenths(camera_joint_can_instance->current_pan_deg);
-
-    int16_t tilt_tenths =
-        DegreesToTenths(camera_joint_can_instance->current_tilt_deg);
-
-    CAN_WriteInt16LE(data, 0U, pan_tenths);
-    CAN_WriteInt16LE(data, 2U, tilt_tenths);
-
-    data[4] = CAMERA_JOINT_STATUS_OK;
-    data[5] = 0U;
-    data[6] = 0U;
-    data[7] = 0U;
-
-    return CAN_TRANSMIT(
-        hcan,
-        CAN_CAMERA_JOINT_STATUS_ID,
-        data,
-        CAN_CAMERA_JOINT_STATUS_DLC
-    );
-}
